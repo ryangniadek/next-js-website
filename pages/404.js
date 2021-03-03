00fd9d4f437d296feaadd0a4274f7d5398b41e4b
Ryan Gniadek <ryan@gniadek.net>
2021-03-02 20:51:19 -0500

    404 page updated

--- pages/404.js
+++ pages/404.js
@@ -1,10 +1,18 @@
-
-import Head from 'next/head'
-import Layout from '../components/layout'
+import Head from "next/head";
+import Layout from "../components/layout";
 export default function Custom404() {
   return (
     <Layout>
       <h1>404 - Page Not Found</h1>
+      <p>I guess you still haven't found what you are looking for...</p>
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/e3-5YC_oHjE"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+      ></iframe>
     </Layout>
   );
 }
