f8be3c7b4da5c813ac841eb7d545bc24df7688eb
Ryan Gniadek <ryan@gniadek.net>
2021-04-02 16:59:40 -0400

    Add political resume

--- next.config.js
+++ next.config.js
@@ -6,6 +6,11 @@ module.exports = {
           destination: 'https://docs.google.com/document/d/1NzTWgMwNbMZQWcV3C73mEa8xHUr-D3-CZPg45Ebhfd4/export?format=pdf',
           permanent: true,
         },
+	{
+	  source: '/political-resume',
+	  destination: 'https://docs.google.com/document/d/1cyVwt3XPnN0hnBGgElY7C1yqvCKt5yE4PWXr9vgE0aw/export?format=pdf',
+	  permanent: true,	
+	},
       ]
     },
   }
