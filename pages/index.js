ba76afb29175ace480322233ed51072a4bda8449
Ryan Gniadek <ryan@gniadek.net>
2021-05-11 17:06:37 -0400

    DOF

--- pages/index.js
+++ pages/index.js
@@ -14,26 +14,35 @@ export default function Home({ allPostsData }) {
       <section className={utilStyles.headingMd}>
         <h3>My name is Ryan Gniadek</h3>
         <p>
-          <Link href={`/files/publickey.ryan@gniadek.net.asc`}><a>Click here to download my PGP public key</a></Link>
+          <Link href={`/files/publickey.ryan@gniadek.net.asc`}>
+            <a>Click here to download my PGP public key</a>
+          </Link>
         </p>
         <p>
-          I am a student at the Virginia Tech <Link href="https://eng.vt.edu/"><a>College of Engineering</a></Link>, working
-          towards a Bachelor of Science degree in <Link href="https://cs.vt.edu/"><a>Computer Science</a></Link>.
-          Academically, I am interested in software engineering, programming
+          I am a student at the Virginia Tech{" "}
+          <Link href="https://eng.vt.edu/">
+            <a>College of Engineering</a>
+          </Link>
+          , working towards a Bachelor of Science degree in{" "}
+          <Link href="https://cs.vt.edu/">
+            <a>Computer Science</a>
+          </Link>
+          . Academically, I am interested in software engineering, programming
           languages, and computer systems.{" "}
         </p>
         <p>
-          On campus, I am the President of the{" "}
-          <Link href="https://vtluug.org">
-            <a>Linux and Unix Users Group</a>
-          </Link>{" "}
-          , a group that promotes *nix and free software on campus.
-          Additionally, I serve on the Executive Board of the{" "}
+          On campus, I serve as the Director of Finance for the{" "}
           <Link href="https://www.sec.vt.edu">
             <a>Student Engineers' Council</a>
           </Link>
           , a group that represents undergraduate students in the College of
-          Engineering.{" "}
+          Engineering. I administer our budget with approximately $500k in
+          annual revenue and oversee our endowments with a combined principal of
+          $2.2M. Additionally, I am the President of the{" "}
+          <Link href="https://vtluug.org">
+            <a>Linux and Unix Users Group</a>
+          </Link>
+          , a group that promotes *nix and free software on campus.
         </p>
         <p>
           This is my website where you can see more information about me, check
