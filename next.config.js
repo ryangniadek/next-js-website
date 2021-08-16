79d5e1df9be7851c495cb0ea8bbf1e0e1a3b99b3
Ryan Gniadek <ryan@gniadek.net>
2021-08-16 19:37:35 -0400

    Add playlist url

--- next.config.js
+++ next.config.js
@@ -11,6 +11,11 @@ module.exports = {
 	  destination: 'https://docs.google.com/document/d/1cyVwt3XPnN0hnBGgElY7C1yqvCKt5yE4PWXr9vgE0aw/export?format=pdf',
 	  permanent: true,	
 	},
+	  {
+	  source: '/playlist',
+	  destination: 'https://open.spotify.com/playlist/2yigOU680KQPLpe1Eu0C9y?si=7bb9c4332bc44609',
+	  permanent: true,	
+	},
       ]
     },
   }
