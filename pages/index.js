40ef7d154b171ec31c304a1f3b4e962ef40e3c3a
Ryan Gniadek <rgniadek@Ryans-MacBook-Air.local>
2022-03-04 21:15:19 -0500

    Removed PGP key

--- pages/index.js
+++ pages/index.js
@@ -14,11 +14,6 @@ export default function Home({ allPostsData }) {
       <section className={utilStyles.headingMd}>
         <h3>My name is Ryan Gniadek</h3>
         <p>
-          <Link href={`/files/publickey.ryan@gniadek.net.asc`}>
-            <a>Click here to download my PGP public key</a>
-          </Link>
-        </p>
-        <p>
           I am a student at the Virginia Tech{" "}
           <Link href="https://eng.vt.edu/">
             <a>College of Engineering</a>
