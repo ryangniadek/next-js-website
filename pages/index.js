3bc7c8ff8c63a5e5e43301c1f92bcee222337307
Ryan Gniadek <rgniadek@Ryans-MacBook-Air.local>
2022-05-02 23:46:51 -0400

    Added publications template

--- pages/index.js
+++ pages/index.js
@@ -52,6 +52,16 @@ export default function Home({ allPostsData }) {
         </p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Publications</h2>
+        <ul className={utilStyles.list}>
+            <li className={utilStyles.listItem}>
+              <Link href="#">
+                <a>Test</a>
+              </Link>
+            </li>
+        </ul>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
