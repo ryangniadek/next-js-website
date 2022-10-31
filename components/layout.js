7e8e3e13fb20693aa3858aa7aed331274d5783ac
Ryan Gniadek <ryan.gniadek@anedot.com>
2022-10-31 00:17:55 -0400

    Fixed header

--- components/layout.js
+++ components/layout.js
@@ -21,7 +21,23 @@ export default function Layout({ children, home }) {
         <meta name="twitter:card" content="summary_large_image" />
       </Head>
       <header className={styles.header}>
-        {home ? (
+      <>
+        <Link href="/">
+          <a>
+            <img
+              src="/images/profile.jpg"
+              className={`${styles.headerImage} ${utilStyles.borderCircle}`}
+              alt={name}
+            />
+          </a>
+        </Link>
+        <h2 className={utilStyles.headingLg}>
+          <Link href="/">
+            <a className={utilStyles.colorInherit}>{name}</a>
+          </Link>
+        </h2>
+      </>
+        {/* {home ? (
           <>
             <img
               src="/images/profile.jpg"
@@ -47,7 +63,7 @@ export default function Layout({ children, home }) {
               </Link>
             </h2>
           </>
-        )}
+        )} */}
         <Menu></Menu>
       </header>
       <main>{children}</main>
