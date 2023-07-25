ccdcf927df6332ea7c0356f1320a756e091c7b30
Ryan Gniadek <rpg@redhat.com>
2023-07-25 15:12:03 -0400

    Certs redirect

--- next.config.js
+++ next.config.js
@@ -6,16 +6,26 @@ module.exports = {
           destination: 'https://docs.google.com/document/d/1NzTWgMwNbMZQWcV3C73mEa8xHUr-D3-CZPg45Ebhfd4/export?format=pdf',
           permanent: true,
         },
-	{
-	  source: '/playlist',
-	  destination: 'https://open.spotify.com/playlist/2yigOU680KQPLpe1Eu0C9y?si=7bb9c4332bc44609',
-	  permanent: true,	
-	},
-	{
-	  source: '/calendar',
-	  destination: 'https://calendly.com/rgniadek',
-	  permanent: true,	
-	}, 
+        {
+          source: '/playlist',
+          destination: 'https://open.spotify.com/playlist/2yigOU680KQPLpe1Eu0C9y?si=7bb9c4332bc44609',
+          permanent: true,	
+        },
+        {
+          source: '/calendar',
+          destination: 'https://calendly.com/rgniadek',
+          permanent: true,	
+        }, 
+        {
+          source: '/certs',
+          destination: 'https://credly.com/users/ryangniadek',
+          permanent: true,
+        },
+        {
+          source: '/certifications',
+          destination: '/certs',
+          permanent: true,
+        },
       ]
     },
   }
