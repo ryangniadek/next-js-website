f54e448283d9dcc0b15a73f0dfca372470f286f3
Ryan Gniadek <ryan@gniadek.net>
2024-08-30 21:12:32 -0400

    Removed API routes

--- pages/api/[pong].ts
+++
@@ -1,7 +0,0 @@
-import { NextApiRequest, NextApiResponse } from 'next'
-
-export default (req: NextApiRequest, res: NextApiResponse) => {
-  const { pong } = req.query
-  res.statusCode = 200
-  res.json({ ping: pong })
-}
--- pages/api/hello/[world].ts
+++
@@ -1,7 +0,0 @@
-import { NextApiRequest, NextApiResponse } from 'next'
-
-export default (req: NextApiRequest, res: NextApiResponse) => {
-  const { world } = req.query
-  res.statusCode = 200
-  res.json({ hello: world })
-}
