e2a28afd5e7f5710007fb96ed370051872e47123
Ryan Gniadek <ryan@gniadek.net>
2025-05-14 14:48:29 -0400

    Added wedding redirect

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@ module.exports = {
     async redirects() {
       return [
         {
+          source: '/wedding',
+          destination: 'https://wedding.gniadek.net',
+          permanent: true,
+        },
+        {
           source: '/resume',
           destination: 'https://docs.google.com/document/d/1NzTWgMwNbMZQWcV3C73mEa8xHUr-D3-CZPg45Ebhfd4/export?format=pdf',
           permanent: true,
