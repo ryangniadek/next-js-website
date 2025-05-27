ba221783dd7b554fcc9a2f5606be327137195a5a
Ryan Gniadek <ryan@gniadek.net>
2025-05-27 15:49:42 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -7,6 +7,11 @@ module.exports = {
           permanent: true,
         },
         {
+          source: '/registry',
+          destination: 'https://registry.gniadek.net',
+          permanent: true,
+        },
+        {
           source: '/resume',
           destination: 'https://docs.google.com/document/d/1NzTWgMwNbMZQWcV3C73mEa8xHUr-D3-CZPg45Ebhfd4/export?format=pdf',
           permanent: true,
